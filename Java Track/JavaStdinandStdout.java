977e6e546be11385e036d67b0cc477db72d470da
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-05-03 17:16:06 -0700

    formatting

--- Java Track/JavaStdinandStdout.java
+++ Java Track/JavaStdinandStdout.java
@@ -38,22 +38,19 @@ Sample Output
 125
  */
 
- import java.util.*;
+ import java.util.Scanner;
 
  public class Solution {
 
      public static void main(String[] args) {
          Scanner scan = new Scanner(System.in);
-         int a = scan.nextInt();
-         int b = scan.nextInt();
-         int c = scan.nextInt();
+         int i = scan.nextInt();
+         double d = scan.nextDouble();
+         scan.nextLine();
+         String s = scan.nextLine();
 
-
-
-         System.out.println(a);
-         System.out.println(b);
-         System.out.println(c);
-
-
+         System.out.println("String: " + s);
+         System.out.println("Double: " + d);
+         System.out.println("Int: " + i);
      }
  }
