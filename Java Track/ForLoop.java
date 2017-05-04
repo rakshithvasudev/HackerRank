8bb5b52963607a1983bb7bf4f3d015606f8167d7
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-05-03 19:41:11 -0700

    Data types

--- Java Track/ForLoop.java
+++ Java Track/ForLoop.java
@@ -36,20 +36,27 @@ Sample Output
 2 x 10 = 20
 */
 
-import java.io.*;
 import java.util.*;
-import java.text.*;
+import java.io.*;
 import java.math.*;
-import java.util.regex.*;
 
-public class Solution {
-
-    public static void main(String[] args) {
+class Solution{
+    public static void main(String []argh){
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
 
-        for(int i=1;i<11;i++)
-            System.out.println(N +" x "+i + " = "+N*i);
-
+            int prev=a, current;
+            for(int j=0;j<n;j++){
+                current = prev+(int)(Math.pow(2,j))*b;
+                System.out.print(current +" ");
+                prev = current;
+             }
+             System.out.println();
+          }
+        in.close();
     }
 }
