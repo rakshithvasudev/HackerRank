37babdc7f10e83ef3625ee06ab6093dd99092361
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-05-06 23:07:17 -0700

    Added more strings

--- Java Track/Strings/StringsIntro.java
+++ Java Track/Strings/StringsIntro.java
@@ -37,7 +37,7 @@ String  is "hello" and  is "java".
  has a length of , and  has a length of ; the sum of their lengths is .
 When sorted alphabetically/lexicographically, "hello" comes before "java"; therefore,  is not larger than  and the answer is No.
 
-When you capitalize the first letter of both  and  and then print them separated by a space, you get "Hello Java".
+When you capitalize the letter of both  and  and then print them separated by a space, you get "Hello Java".
 
 Submissions: 48000
 Max Score: 5
