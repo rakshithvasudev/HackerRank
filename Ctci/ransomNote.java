625c1e568b178e44927ec9a791696fe8bd860ff0
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-06-03 22:35:26 -0700

    added ransom partial solution

--- Ctci/ransomNote.java
+++ Ctci/ransomNote.java
@@ -29,3 +29,46 @@ Yes
 
 */
 
+import java.util.*;
+
+public class Solution {
+    List<String> magElements;
+    List<String> noteElements;    
+    
+    public Solution(String magazine, String note) {
+        magElements = new ArrayList();
+        noteElements = new ArrayList();
+
+        for(String s: magazine.split(" "))
+            magElements.add(s);
+         for(String s: note.split(" "))
+            noteElements.add(s);
+    }
+    
+    public boolean solve() {
+        for(String s: noteElements){
+            if(!magElements.contains(s)){
+              return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        
+        // Eat whitespace to beginning of next line
+        scanner.nextLine();
+        
+        Solution s = new Solution(scanner.nextLine(), scanner.nextLine());
+        scanner.close();
+        
+        boolean answer = s.solve();
+        if(answer)
+            System.out.println("Yes");
+        else System.out.println("No");
+      
+    }
+}
