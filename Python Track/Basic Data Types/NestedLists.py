be77f771f5d5aef515c02b4ab05c5c0e029a4b6d
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-06-05 16:43:51 -0700

    added solution

--- Python Track/Basic Data Types/NestedLists.py
+++ Python Track/Basic Data Types/NestedLists.py
@@ -40,3 +40,39 @@ students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], 
 The lowest grade of  belongs to Tina. The second lowest grade of  belongs to both Harry and Berry, so we order their names alphabetically and print each name on a new line.
 '''
 
+if __name__ == '__main__':
+    students = []
+    sortedNames = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name, score])
+    # trying to implement without sort
+    # students = sorted(students,key=lambda students:students[1])
+
+    # get the least valued element
+    minVal = min(students, key=lambda students: students[1])
+
+    # if there are more than one min valued elements, delete them
+    # as long as the previously min element defined == the actual least element
+    while minVal == min(students, key=lambda students: students[1]):
+        students.remove(min(students, key=lambda students: students[1]))
+
+    # capture the value of that element
+    valueReq = min(students, key=lambda students: students[1])[1]
+
+    # print the name of second lowest element(required)
+    # print(min(students, key = lambda students:students[1])[0])
+    sortedNames.append(min(students, key=lambda students: students[1])[0])
+    students.remove(min(students, key=lambda students: students[1]))
+
+    # print all the required elements, i.e delete the already printed element
+    while (valueReq == min(students, key=lambda students: students[1])[1]):
+        # print(min(students, key = lambda students:students[1])[0])
+        sortedNames.append(min(students, key=lambda students: students[1])[0])
+        students.remove(min(students, key=lambda students: students[1]))
+
+    sortedNames.sort()
+
+    for name in sortedNames:
+        print(name)
