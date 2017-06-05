bc7d920b787cd46978142d43b83112e4283f5c6c
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-06-05 10:56:36 -0700

    added print func

---
+++ Python Track/Introduction/Printfunc.py
@@ -0,0 +1,22 @@
+'''
+Read an integer .
+
+Without using any string methods, try to print the following:
+
+
+Note that "" represents the values in between.
+
+Input Format 
+The first line contains an integer .
+
+Output Format 
+Output the answer as explained in the task.
+
+Sample Input
+
+3
+Sample Output
+
+123
+'''
+
