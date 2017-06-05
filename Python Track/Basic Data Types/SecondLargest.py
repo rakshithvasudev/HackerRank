40811412382f4cf1030546affc0ecad4a5115e2c
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-06-05 12:55:56 -0700

    added second largest

---
+++ Python Track/Basic Data Types/SecondLargest.py
@@ -0,0 +1,33 @@
+'''
+Find the Second Largest Number
+You are given  numbers. Store them in a list and find the second largest number.
+
+Input Format 
+The first line contains . The second line contains an array [] of  integers each separated by a space.
+
+Constraints 
+ 
+
+Output Format 
+Output the value of the second largest number.
+
+Sample Input
+
+5
+2 3 6 6 5
+Sample Output
+
+5
+'''
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    arr = list(arr)
+    arr.sort(reverse=True)
+    max = arr[0]
+    i = 1
+    while arr[i] == max:
+        i += 1
+
+    print(arr[i])
