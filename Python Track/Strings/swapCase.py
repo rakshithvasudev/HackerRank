7e77e145d573fe7e41cdc43ba94d837b7ed659f7
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-06-05 18:40:10 -0700

    added swap case

---
+++ Python Track/Strings/swapCase.py
@@ -0,0 +1,26 @@
+'''
+You are given a string . Your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
+
+For Example:
+
+Www.HackerRank.com → wWW.hACKERrANK.COM
+Pythonist 2 → pYTHONIST 2
+Input Format
+
+A single line containing a string .
+
+Constraints
+
+
+Output Format
+
+Print the modified string .
+
+Sample Input
+
+HackerRank.com presents "Pythonist 2".
+Sample Output
+
+hACKERrANK.COM PRESENTS "pYTHONIST 2".
+'''
+
