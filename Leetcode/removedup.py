cac773f027ca75e131ba2c1d0b5999a7e301afd0
Rakshith Vasudev <rakshithvbharani@gmail.com>
2018-12-09 21:04:30 -0800

    add comments

--- Leetcode/removedup.py
+++ Leetcode/removedup.py
@@ -4,12 +4,19 @@ def removeDuplicates(nums):
     :rtype: int
     """
     
-    
+    # keeps a track of all the unique indices
     unq = 0
     for idx in range(len(nums)-1):
         if nums[idx] != nums[idx+1]:
+            
+            # replace current numbers with unique indices so that there isn't any duplicates: replace the last occurance of the duplicated element with the
+            # first occurance of the same duplicated element
+            # ex: [1,2,2,2,3,4] => 2 in index 3 replaces 2 in index 1.
+            # j keeps a track of that index 1 and idx keeps a track of index 3.   
             nums[unq] = nums[idx]
+            
             unq+=1
+
     nums[unq] = nums[-1]
     return unq+1, nums
 print(removeDuplicates([1,2,2,3,3]))
